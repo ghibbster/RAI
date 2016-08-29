c112128269eb17f1c31b0b640a5a7f16ba524efc
ghibbster <gllpellegrino@gmail.com>
2016-08-29 11:13:38 +0200

    copyright messages

--- RedState.java
+++ RedState.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2016, Gaetano Pellegrino
+ *
+ * This program is released under the GNU General Public License
+ * Info online: http://www.gnu.org/licenses/quick-guide-gplv3.html
+ * Or in the file: LICENSE
+ * For information/questions contact: gllpellegrino@gmail.com
+ */
+
 package RAI;
 
 import RAI.transition_clustering.ClusteredTransition;
